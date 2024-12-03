6fbda5dac6046ffd7c10d351a3886cca565d882f
bluefalconhd <hjdombro@icloud.com>
2024-12-03 07:37:16 -0600

    feat: config unmarshaling

--- config.go
+++ config.go
@@ -1,6 +1,29 @@
 package main
 
-// TODO: REDO CONFIG SYSTEM
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
 
 type Config struct {
+	ConfigFilePath string
+	FragmentsPath  string `yaml:"fragments"`
+	PagePath       string `yaml:"pages"`
+	BuildPath      string `yaml:"build"`
+}
+
+func GetConfiguration(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{}
+	err = yaml.Unmarshal([]byte(data), cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.ConfigFilePath = path
+	return cfg, nil
 }
